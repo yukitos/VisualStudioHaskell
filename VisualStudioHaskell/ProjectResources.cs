a600940406e49880b63b72670117f029d58ae47c
quartorz <tsuhaso@gmail.com>
2015-04-10 02:08:09 +0900

    add classifier (not completed)

---
+++ VisualStudioHaskell/ProjectResources.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Globalization;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Company.VisualStudioHaskell
+{
+    internal class ProjectResources
+    {
+        public const string ReservedWordClassificationType = "ReservedWordClassificationType";
+
+        private static readonly Lazy<ResourceManager> _manager = new Lazy<ResourceManager>(
+            () => new ResourceManager("Microsoft.VisualStudio.Project", typeof(ProjectResources).Assembly),
+            LazyThreadSafetyMode.ExecutionAndPublication
+        );
+
+        private static ResourceManager Manager
+        {
+            get
+            {
+                return _manager.Value;
+            }
+        }
+
+        private static string GetStringInternal(string value, object[] args)
+        {
+            string result = Manager.GetString(value, CultureInfo.CurrentUICulture);
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            if (args.Length == 0)
+            {
+                Debug.Assert(value.IndexOf("{0}") < 0);
+                return result;
+            }
+
+            return string.Format(CultureInfo.CurrentUICulture, result, args);
+        }
+
+        public static string GetString(string value, object[] args)
+        {
+            var result = GetStringInternal(value, args);
+            Debug.Assert(result != null);
+            return result ?? value;
+        }
+
+        internal static string GetUnhandledExceptionString(
+            Exception ex,
+            Type callerType,
+            [CallerFilePath] string callerFile = null,
+            [CallerLineNumber] int callerLineNumber = 0,
+            [CallerMemberName] string callerName = null
+            )
+        {
+            // not implemented
+            return "";
+        }
+    }
+}
